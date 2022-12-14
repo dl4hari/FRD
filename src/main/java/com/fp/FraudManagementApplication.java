04e0f87e4ee58820cfeef379ee530da08202eb60
harikrishnap <hari.krishna@solix.com>
2022-12-15 01:42:57 +0530

    swagger commit

--- src/main/java/com/fp/FraudManagementApplication.java
+++ src/main/java/com/fp/FraudManagementApplication.java
@@ -1,11 +1,15 @@
 package com.fp;
 
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.info.Info;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @SpringBootApplication
 @EnableJpaRepositories("com.fp.repository")
+@OpenAPIDefinition(info = @Info(title = "Fraud Master API",
+        version = "3.0", description = "Fraud Master API Information"))
 public class FraudManagementApplication {
 
     public static void main(String[] args) {
